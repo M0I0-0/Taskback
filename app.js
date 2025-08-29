ea99994b939b376ce4782631edfb61883dc3d901
Moi <casanovamoi166@gmail.com>
2025-08-29 14:23:14 -0600

    commit 2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import authRoutes from "./src/routes/auth.routes.js";
 import taskRoutes from "./src/routes/task.routes.js";
 import hoursRoutes from "./src/routes/hours.routes.js";
@@ -8,24 +8,23 @@ import scheduleRoutes from "./src/routes/schedule.routes.js";
 import notes from "./src/routes/notes.routes.js";
 import eventos from "./src/routes/eventos.routes.js";
 
-import dotenv from 'dotenv';
-import e from 'express';
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/tasks', taskRoutes);
-app.use("/api/horas", hoursRoutes);
-app.use("/api/materias", subjectsRoutes);
-app.use("/api/horario", scheduleRoutes);
-app.use("/api/notes", notes);
-app.use("/api/eventos", eventos);
-app.use('/api/auth', authRoutes);
-
+app.use("/tasks", taskRoutes);
+app.use("/horas", hoursRoutes);
+app.use("/materias", subjectsRoutes);
+app.use("/horario", scheduleRoutes);
+app.use("/notes", notes);
+app.use("/eventos", eventos);
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API funcionando 🚀");
 });
+
 export default app;
